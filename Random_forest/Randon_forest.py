4f8a53f372f0d2b54e8020a146d65d499301bdc8
Hanyuan0908 <78325147+Hanyuan0908@users.noreply.github.com>
2021-03-21 12:32:10 +0000

    Add files via upload

--- Random_forest/Randon_forest.py
+++ Random_forest/Randon_forest.py
@@ -4,21 +4,21 @@ import pandas as pd
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.feature_selection import SelectFromModel
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import RandomForestClassifier
-# from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import GridSearchCV
+from sklearn.feature_selection import RFECV
 import joblib
 import matplotlib.pyplot as plt
 #%%
 '''
 read data
 '''
-data = pd.read_csv("Data/Train_data.csv")
+data = pd.read_csv("../Data/Train_data.csv")
 
 X = data.iloc[:,0:8] #Input argument
 Y = data.iloc[:,8] #Output argument
 
-#data.dropna(inplace = True)
+#data.dropna(inplace = True) #if there is any NA block
 '''
 Split data into train and test group with 0.9 and 0.1 randomly
 '''
@@ -53,49 +53,87 @@ for i in range(len(featuresSupport)):
     if featuresSupport[i] == True:
         selecred_feature_name.append(feature_name[i])
 print(selecred_feature_name)
+#%%
 '''
 Creat many random forest model with different split and leaf size, 
 run each forest with some sample data, and leave the best performed one
 '''
-gs = GridSearchCV(
+gs_b = GridSearchCV(
     param_grid = {'min_samples_leaf':np.linspace(5,55,10).astype(int),
                   'min_samples_split':np.linspace(5,55,10).astype(int)},
     estimator = RandomForestClassifier(n_estimators=1000),
     scoring = 'accuracy')#The selection of the best model is based on the accuracy, other candidate are 'f1', 'balanced_accuracy', 'roc_auc'
-
 '''
 Train the model
 '''
-gs.fit(X_train,Y_train)#Train the model
+gs_b.fit(X_train,Y_train)
+
+# recursive feature elimination and cross-calidated selection of best number of features.
+gsFeatureSelector = RFECV(gs_b.best_estimator_, cv = 5).fit(X_train,Y_train)
+gsX = gsFeatureSelector.transform(X_train)
+gsFeatureSupport = gsFeatureSelector.support_
+
+gs_a = GridSearchCV(
+    param_grid = {'min_samples_leaf':np.linspace(5,55,10).astype(int),
+                  'min_samples_split':np.linspace(5,55,10).astype(int)},
+    estimator = RandomForestClassifier(n_estimators=1000),
+    scoring = 'accuracy')
+gs_a.support = gsFeatureSupport
+gs_a.selector = gsFeatureSelector
+gs_a.fit(gsX,Y_train)#Train the model
+
+#%%
+# Linear Regression model and feature selection
+linearRegression = LinearRegression()
+linearFeatureSelector = RFECV(linearRegression, cv = 5).fit(X_train,Y_train)
+LinearX = linearFeatureSelector.transform(X_train)
+linearFeatureSupport = linearFeatureSelector.support_
+# store selector in Linear Regression Model
+linearRegression.support = linearFeatureSupport
+linearRegression.selector = linearFeatureSelector
+
+# train Linear Regression Model
+linearRegression.fit(LinearX,Y_train)
 
 # %%
-"""
-Varification
-"""
-varifi_data = pd.read_csv("Data/Verification_data.csv")
 
-X_ver = data.iloc[:,0:8] #Input argument
-Y_ver = data.iloc[:,8] #Output argument
+'''
+We can choose to open or not open a model
+'''
+#with open('\Model_RandomForest.joblib', 'rb') as gs_a:
+
+#gs_a = joblib.load('./Model_RandomForest.joblib')
+
+
+"""
+Verification
+"""
+verifi_data = pd.read_csv("../Data/Verification_data.csv")
+
+X_ver = verifi_data.iloc[:,0:8] #Input argument
+Y_ver = verifi_data.iloc[:,8] #Output argument
 
 X_ver = poly.fit_transform(X_ver)#Generate test feature
 X_ver = select.transform(X_ver)#Selecr test feature
 
+X_gs = gs_a.selector.transform(X_ver)
+X_linear = linearRegression.selector.transform(X_ver)
 
-print(gs.score(X_ver,Y_ver))#print the accuracy of the result
+print(gs_a.score(X_gs,Y_ver))#print the accuracy of the random forest result
+print(linearRegression.score(X_linear,Y_ver))#print the accuracy of the linear regression result
+
+result = gs_a.predict(X_gs)
+np.savetxt('RandomForest_Prediction.txt',result,fmt = '%d') #if we want to export our prediction
+print(gs_a.predict(X_gs))# print the prediction of each row
 
 
-print(gs.predict(X_ver))# print the prediction of each row
-
-
-weight = gs.best_estimator_.feature_importances_
-print(gs.best_estimator_.feature_importances_)#print the statistical weight of each feature in the forest
+weight = gs_a.best_estimator_.feature_importances_
+print(gs_a.best_estimator_.feature_importances_)#print the statistical weight of each feature in the forest
 #%%
 '''
 save the model if we want
 '''
-#joblib.dump(gs, 'model')
-
-
+#joblib.dump(gs_a, 'Model_RandomForest.joblib')
 
 
 #%% 
@@ -104,7 +142,7 @@ Visulisation part starts from here
 '''
 #%%
 '''
-Statistical Weight diagram (bar chart or pie chart)
+Statistical Weight diagram (bar chart)
 '''
 #Bar chart
 params = {
@@ -124,52 +162,6 @@ ax1.set_xticks(3*np.linspace(1, len(weight),len(weight)))
 ax1.set_xticklabels(selecred_feature_name)
 ax1.set(ylim = [0,0.2])
 
-# Pie chart
-params = {
-    'axes.labelsize': 20,
-    'font.size':22,
-    'legend.fontsize': 22,
-    'xtick.labelsize': 20,
-    'ytick.labelsize': 20,
-    'figure.figsize': [20,18]
-    } 
-plt.rcParams.update(params)
-plt.rcParams["font.family"]="Times New Roman"
-# Pie chart, where the slices will be ordered and plotted counter-clockwise:
-labels = selecred_feature_name
-sizes = weight
-#explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
-
-fig2, ax2 = plt.subplots()
-ax2.pie(sizes, labels=labels, autopct='%1.1f%%',
-        shadow=False, startangle=90)
-ax2.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-#%% Plot for accuracy
-
-Correct = 0
-Wrong = 0
-
-for i in range(len(Y_test)):
-    if np.array(Y_test)[i] == gs.predict(X_test)[i]:
-        Correct+=1
-    else:
-        Wrong+=1
-
-params = {
-    'axes.labelsize': 20,
-    'font.size':22,
-    'legend.fontsize': 22,
-    'xtick.labelsize': 20,
-    'ytick.labelsize': 20,
-    'figure.figsize': [10,8]
-    }
-plt.rcParams.update(params)
-plt.rcParams["font.family"]="Times New Roman"
- 
-fig3, ax3 = plt.subplots()
-explode = (0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
-ax3.pie([Correct, Wrong], explode=explode, labels=['Correct','Wrong'], autopct='%1.1f%%',
-        shadow=False, startangle=90)
 
 
 
